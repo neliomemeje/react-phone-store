19e846988827b046a57af533e907a656874c7f7e
Nelio Memeje <neliomemeje@yahoo.com>
2023-08-06 14:42:21 +0800

    changing styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../icon.png";
 import { Button } from "./StyledEl";
+import { ProductConsumer } from "../context";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
-      <Link to="/">
-        <img src={logo} alt="logo" className="navbar-brand" />
-      </Link>
-      <ul className="navbar-nav">
-        <li className="nav-item ms-5">
-          <Link to="/" className="nav-link">
-            Products
-          </Link>
-        </li>
-      </ul>
-      <Link to="/cart" className="ms-auto">
-        <Button className="cart-btn">
-          <span className="me-2">
-            <i className="fas fa-cart-plus" />
-          </span>
-          My Cart
-        </Button>
-      </Link>
-    </nav>
+    <ProductConsumer>
+      {(value) => {
+        const { cart } = value;
+        return (
+          <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <Link to="/">
+              <img src={logo} alt="logo" className="navbar-brand" />
+            </Link>
+            <ul className="navbar-nav">
+              <li className="nav-item ms-5">
+                <Link to="/" className="nav-link">
+                  Products
+                </Link>
+              </li>
+            </ul>
+            <Link to="/cart" className="ms-auto">
+              <Button className="cart-btn">
+                <span id="cart-link" className="me-2">
+                  <i className="fas fa-cart-plus" />
+                  <div className="cart-item">{cart.length}</div>
+                </span>
+                My Cart
+              </Button>
+            </Link>
+          </nav>
+        );
+      }}
+    </ProductConsumer>
   );
 }
 
